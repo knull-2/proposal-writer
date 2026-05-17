a8eca224b19d16fa5219368057ac975cfed2c8b0
knull-2 <vishvesh.rps@gmail.com>
2026-05-17 13:13:25 +0530

    Delete start.bat

--- start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "C:\Users\Ravi prakash\proposal-writer"
-call venv\Scripts\activate
-set GEMINI_API_KEY=AIzaSyBTS4S3-BJbwjhh-ojhRxhoowpJd7V2JAs
-echo Starting AI Proposal Writer Pro...
-start http://localhost:8000
-uvicorn main:app --port 8000
-pause
